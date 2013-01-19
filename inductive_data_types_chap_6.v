b7fbafcd65ad948e10d5fdd3571e6ecdb8a1d268
Corey O'Connor <corey@ooyala.com>
2013-01-19 14:31:59 -0800

    up to ex 6.12

--- inductive_data_types_chap_6.v
+++ inductive_data_types_chap_6.v
@@ -102,60 +102,58 @@ Proof.
   reflexivity.
 Qed.
 
-Section six_1_3.
-  Definition f_0 (b : bool) :=
-    match b with
-    | true  => 1
-    | false => 2
-    end.
+Definition f_0 (b : bool) :=
+  match b with
+  | true  => 1
+  | false => 2
+  end.
 
-  Print f_0.
+Print f_0.
 
-  Definition month_length (leap : bool) (m : month) : nat :=
-    match m with
-    | January  => 31
-    | February => if leap then 29 else 28
-    | March => 31
-    | April => 30
-    | May => 31
-    | June => 30
-    | July => 31
-    | August => 31
-    | September => 30
-    | October => 31
-    | November => 30
-    | December => 31
-    end.
+Definition month_length (leap : bool) (m : month) : nat :=
+  match m with
+  | January  => 31
+  | February => if leap then 29 else 28
+  | March => 31
+  | April => 30
+  | May => 31
+  | June => 30
+  | July => 31
+  | August => 31
+  | September => 30
+  | October => 31
+  | November => 30
+  | December => 31
+  end.
 
-  Check month_rec.
-  Definition month_length' (leap : bool) : month -> nat :=
-    month_rec (fun (m : month) => nat)
-              31
-              (if leap then 29 else 28)
-              31
-              30
-              31
-              30
-              31
-              31
-              30
-              31
-              30
-              31.
+Check month_rec.
+Definition month_length' (leap : bool) : month -> nat :=
+  month_rec (fun (m : month) => nat)
+            31
+            (if leap then 29 else 28)
+            31
+            30
+            31
+            30
+            31
+            31
+            30
+            31
+            30
+            31.
 
-  Definition month_length'' (leap : bool) (m : month) : nat :=
-    match m with
-    | February => if leap then 29 else 28
-    | April => 30
-    | June => 30
-    | September => 30
-    | November => 30
-    | otherwise => 31
-    end.
+Definition month_length'' (leap : bool) (m : month) : nat :=
+  match m with
+  | February => if leap then 29 else 28
+  | April => 30
+  | June => 30
+  | September => 30
+  | November => 30
+  | otherwise => 31
+  end.
 
-  Eval compute in month_length'' false April.
+Eval compute in month_length'' false April.
 
-End six_1_3.
 
 Section ex_6_6.
   Definition bool_and (b1 b2 : bool) : bool :=
@@ -348,3 +346,185 @@ Definition nb_seats' (v : vehicle) : nat :=
               v.
 
 Print nb_seats'.
+
+Open Scope nat_scope.
+
+Theorem at_least_28 : forall (leap:bool) (m:month), 28 <= month_length leap m.
+Proof.
+  intros leap m.
+  case m ; simpl ; auto with arith.
+  case leap; simpl ; auto with arith.
+Qed.
+
+Reset at_least_28.
+
+Theorem at_least_28 : forall (leap:bool) (m:month), 28 <= month_length leap m.
+Proof.
+  intros leap m.
+  case m.
+  simpl month_length.
+  Check le_n.
+  Check le_S.
+  apply le_S.
+  apply le_S.
+  apply le_S.
+  apply le_n.
+  simpl month_length.
+  case leap.
+  apply le_S.
+  apply le_n.
+  apply le_n.
+  simpl month_length.
+  apply le_S.
+  apply le_S.
+  apply le_S.
+  apply le_n.
+  simpl month_length.
+  apply le_S.
+  apply le_S.
+  apply le_n.
+  simpl month_length.
+  apply le_S.
+  apply le_S.
+  apply le_S.
+  apply le_n.
+  simpl month_length.
+  apply le_S.
+  apply le_S.
+  apply le_n.
+  simpl month_length.
+  apply le_S.
+  apply le_S.
+  apply le_S.
+  apply le_n.
+  simpl month_length.
+  apply le_S.
+  apply le_S.
+  apply le_S.
+  apply le_n.
+  simpl month_length.
+  apply le_S.
+  apply le_S.
+  apply le_n.
+  simpl month_length.
+  apply le_S.
+  apply le_S.
+  apply le_S.
+  apply le_n.
+  simpl month_length.
+  apply le_S.
+  apply le_S.
+  apply le_n.
+  simpl month_length.
+  apply le_S.
+  apply le_S.
+  apply le_S.
+  apply le_n.
+Qed.
+
+(* that was tiring *)
+Print at_least_28.
+Print le_S.
+
+Definition next_month (m : month) : month :=
+  match m with
+      January => February | February  => March     | March     => April
+  |   April   => May      | May       => June      | June      => July
+  |   July    => August   | August    => September | September => October
+  |   October => November | November  => December  | December  => January
+  end.
+
+(* this version is way less interesting. *)
+Theorem next_auguest_then_july_boring :
+  forall m : month, m = July -> next_month m = August.
+Proof.
+  intros m.
+  intros H_m_eq_july.
+  rewrite -> H_m_eq_july.
+  simpl.
+  reflexivity.
+Qed.  
+
+Theorem next_august_then_july :
+  forall m : month, next_month m = August -> m = July.
+Proof.
+  intros m.
+  case m ; simpl ; intros H_next_eq; discriminate H_next_eq || reflexivity.
+  (* The above was very compact, but I think some connectives would
+improve understandably. *)
+Qed.
+
+(* I am having a hard time understanding the evidence function of this: *)
+Print next_august_then_july.
+(* a bit complex no? *)
+
+Theorem not_January_eq_February : ~January = February.
+Proof.
+  unfold not.
+  intros H_january_eq_february.
+  change ((fun m:month => match m with January => True | _ => False end)
+          February).
+  rewrite <- H_january_eq_february.
+  trivial.
+Qed.
+
+Print month_rect.
+
+Definition is_January' (m:month) : Prop :=
+  match m with
+    January => True
+  | _       => False
+  end.
+
+Definition is_January (m:month) : Prop :=
+  month_rect (fun _ => Prop)
+             True
+             False
+             False
+             False
+             False
+             False
+             False
+             False
+             False
+             False
+             False
+             False
+             m.
+
+Eval compute in is_January January.
+Eval compute in is_January March.
+
+Theorem not_true_false : ~true = false.
+Proof.
+  unfold not.
+  intros H_eq.
+  change ((fun (v:bool) => match v with true => True | false => False end)
+          false).
+  rewrite <- H_eq.
+  trivial.
+Qed.
+
+Print not_true_false.
+
+Theorem not_bike_eq_moto : forall (n m : nat), ~(bicycle n = motorized n m).
+Proof.
+  intros n m.
+  unfold not.
+  intro H_eq.
+  change ((fun (v : vehicle) => match v with
+                                  bicycle _ => True
+                                | motorized _ _ => False
+                                end)
+          (motorized n m)).
+  rewrite <- H_eq.
+  trivial.
+Qed.
+
+Theorem not_bike_eq_moto' : forall (n m : nat), ~(bicycle n = motorized n m).
+Proof.
+  intros n m.
+  unfold not.
+  intros H_eq.
+  discriminate H_eq.
+Qed.
